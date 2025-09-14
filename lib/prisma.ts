c0e9c65ae9d49b9b614237a8cafb9fcaad24b2bf
r0hhhan <igrohan3341@gmail.com>
2025-09-14 16:14:51 +0530

    updated db url and added pooling url

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
+export const prisma = globalForPrisma.prisma ?? 
   new PrismaClient({
     log: ['query'],
-  });
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export default prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
